d547f50370c4281d18f325fb020ee347a4d83772
garrettlsfoux <garrettlsfoux@gmail.com>
2023-08-21 15:01:37 -0500

    added .env file reading to development

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,13 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 import { ready, interactionCreate, messageCreate, guildMemberAdd } from './listeners';
 import Database from './db';
+import { config } from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  config({
+    path: '.env',
+  });
+}
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
@@ -13,5 +20,5 @@ ready(client, db);
 interactionCreate(client, db);
 messageCreate(client);
 guildMemberAdd(client);
-
+console.log(process.env.DISCORD_TOKEN);
 client.login(process.env.DISCORD_TOKEN);
