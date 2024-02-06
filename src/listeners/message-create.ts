4603958cc367bbc26d772f9d37e83798f2858d22
garrettlsfoux <garrettlsfoux@gmail.com>
2024-02-06 10:23:48 -0600

    update to node 20, update packages, remove god-react

--- src/listeners/message-create.ts
+++ src/listeners/message-create.ts
@@ -1,9 +1,8 @@
 import { Client, Events } from 'discord.js';
-import { uwu, godReact } from '../tasks';
+import { uwu } from '../tasks';
 
 export default (client: Client): void => {
   client.on(Events.MessageCreate, async (message) => {
     uwu(message);
-    godReact(message);
   });
 };
--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -2,4 +2,3 @@ export { default as uwu } from './uwu';
 export { default as birthdaySchedule } from './birthdays-schedule';
 export { default as greet } from './greet';
 export { default as guildRemove } from './guild-remove';
-export { default as godReact } from './god-react';
