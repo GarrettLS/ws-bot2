bc9c4c6f3970038593c88144b02741728e62a452
garrettlsfoux <garrettlsfoux@gmail.com>
2024-10-29 09:18:11 -0500

    more filters

--- src/tasks/uwu.ts
+++ src/tasks/uwu.ts
@@ -15,7 +15,7 @@ const filter = [
   'uwu-ts-team-uwu', 'wow', 'yato-herzog', 'really-no-way',
   'happy-girls', 'uwu-cute-arjay-gif', 'nYAbPZ6Im4YAAAAC/uwu-cute',
   'hnwblWiTBC0AAAAC/kawaii-cute', 'KXUppoIwoNsAAAAC/uwu-skeleton', 'CCTYyxh2OXoAAAAC/s',
-  'bpVoYKnPJaMAAAAC/pardon-me', 'dragonfish', 'kanna-kamui-kanna-kobayashi'];
+  'bpVoYKnPJaMAAAAC/pardon-me', 'dragonfish', 'kanna-kamui-kanna-kobayashi', 'me-when-the-gang-leaves'];
 const bypassRoles = ['1164320609170374678'];
 
 export default async (message: Message): Promise<void> => {
