f27e1e0260a421452f5dbb0fb09ff4121c03a4d3
garrettlsfoux <garrettlsfoux@gmail.com>
2025-01-26 19:07:46 -0600

    fix medias

--- src/tasks/linkBlocker.ts
+++ src/tasks/linkBlocker.ts
@@ -20,20 +20,17 @@ const medias: IURLModel[] = [
     url: 'truthsocial.com'
   }
 ];
+const fixedMedias: IURLModel[] = common.flatMap(c => {
+  return medias.map(m => {
+    return {
+      name: m.name,
+      url: c + m.url
+    };
+  });
+});
 
 export default async (message: Message): Promise<void> => {
   if (!message.author.bot) {
-    const fixedMedias: IURLModel[] = common.flatMap(c => {
-      return medias.map(m => {
-        return {
-          name: m.name,
-          url: c + m.url
-        };
-      });
-    });
-
-    console.log(fixedMedias);
-
     const found = fixedMedias.find(w => message.content.toLowerCase().includes(w.url));
     if (found) {
       console.log(`linkBlocker: '${message.content}' blocked from ${message.member?.user.id}.`);
